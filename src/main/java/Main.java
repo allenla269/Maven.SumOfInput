4cdf903849954d98f3ce9bf2bf3cd27f796404ef
Allen L <allenl@Zipcoders-MBP.fios-router.home>
2022-02-24 22:36:02 -0500

    final draft

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int numInput = in.nextInt();
+            int sum = 0;
+            for (int i = 1; i <= numInput; i++) {
+                sum += i;
+            }
+            System.out.println("The sum of all numbers from 1 to " + numInput + " is " + sum);
+        }
     }
-}
